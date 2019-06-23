a31cb8d7e95756f86d33e5d192391496390013c3
周子顺 <m18996002212@me.com>
2019-06-23 22:28:34 +0800

    方波/PWM波发生器调试正常，相位问题暂时还没有解决

--- PWMWave.v
+++ PWMWave.v
@@ -1,2 +1,24 @@
-module PWMWave(input clk,input [7:0]PWMDuty,output reg [15:0]PWMout,input reset,input[6:0]phase);
+module PWMWave(input clk,input [6:0]PWMDuty,output reg [15:0]PWMout,input reset,input[6:0]phase);
+
+reg [6:0]address;
+reg [6:0]SynthesisedPhase;
+
+initial
+    begin
+        address=7'd0;
+    end
+
+always@(negedge clk)
+begin
+    address<=address+1'b1;
+    SynthesisedPhase<=address+phase;
+    if (SynthesisedPhase<=PWMDuty) 
+        begin
+            PWMout=16'hFFFF;
+        end 
+    else 
+        begin
+            PWMout=16'h0000;
+        end
+end
 endmodule
--- simulation/modelsim/rtl_work/@p@w@m@wave/_primary.vhd
+++ simulation/modelsim/rtl_work/@p@w@m@wave/_primary.vhd
@@ -3,7 +3,7 @@ use verilog.vl_types.all;
 entity PWMWave is
     port(
         clk             : in     vl_logic;
-        PWMDuty         : in     vl_logic_vector(7 downto 0);
+        PWMDuty         : in     vl_logic_vector(6 downto 0);
         PWMout          : out    vl_logic_vector(15 downto 0);
         reset           : in     vl_logic;
         phase           : in     vl_logic_vector(6 downto 0)
