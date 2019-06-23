9b2528a6334ba9475e638a65e37fffcc99ca5656
周子顺 <m18996002212@me.com>
2019-06-23 13:06:42 +0800

    添加三角波发生和相位合成器，未测试，相位合成器看上去有点问题需要修改

--- TriangularWave.v
+++ TriangularWave.v
@@ -1,2 +1,18 @@
-module TriangularWave(input clk,output reg [15:0]Triangularout,input reset,input[6:0]phase);
+module TriangularWave(input clk,output [15:0]Triangularout,input reset,input[6:0]phase);
+reg [6:0]address;
+reg [6:0]SynthesisedPhase;
+initial
+    begin
+        address=7'd0;
+    end
+
+TriangularROM ROM1(.address(SynthesisedPhase),
+             .clock(clk),
+             .q(Triangularout));
+
+always@(negedge clk)
+    begin
+        address<=address+1'b1;
+        SynthesisedPhase<=address+phase;
+    end
 endmodule
--- phase.v
+++ phase.v
@@ -1,2 +1,16 @@
-module phase (input add,input sub,output [6:0]phase,input reset);
+module phase (input add,input sub,output reg [6:0]phase,input reset);
+
+initial
+    begin
+        phase=7'd0;
+    end
+
+always@(negedge add or negedge sub)
+    begin
+		if(add==0)
+			phase<=phase+7'd1;
+		else if(sub==0)
+			phase<=phase-7'd1;
+    end
+
 endmodule
